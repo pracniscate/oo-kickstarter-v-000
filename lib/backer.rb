905ae42c57019d807c402f319a88916dc2e37f65
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:09:30 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,8 @@
 class Backer
-  attr_accessor :name
-  attr_reader :backed_projects
-
+  attr_accessor :name, :backed_projects
+  
   def initialize(name)
     @name = name
-  end
-
-  def back_project(project)
-    name.backed_projects = []
-  end
-
-  def backed_projects=(name)
-    @backed_projects = name
+    @backed_projects = backed_projects
   end
 end
