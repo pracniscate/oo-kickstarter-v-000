6424636a7fbb57a19e3f8e5a584542e87df0e404
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:17:57 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    # store the project argument in a backed_projects array
+    @backed_projects << project
+    # also add the backer to the project's backers array
+    project.backers << self
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    # store the Backer argument in a backers array
+    @backers << backer
+    # also add the project to the backer's backed_projects array
+    backer.backed_projects << self
+  end
+end
