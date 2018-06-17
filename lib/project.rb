3639fee2a8f6afecd64709b85984916d811b7451
Lorena <lorena.giddens@outlook.com>
2018-06-17 13:13:44 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,12 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers 
 
   def initialize(title)
     @title = title
+    @backers = []
   end
+
+  def add_backer(backer)
+    # store the Backer argument in a backers array
+    @backers << backer
 end 
